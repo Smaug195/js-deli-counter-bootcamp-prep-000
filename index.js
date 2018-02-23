0efb13f45afe3fed11b42d0001b4a9b4193f7734
Eli Draluk <eli.draluk@gmail.com>
2018-02-23 18:54:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(line,nm){
   line.push(nm);
-  return line.length+1
+  return 'Welcome, '+nm+" You are number " + line.length+1 + "in line."
 }
 function nowServing(line){
   if(line.length===0){
