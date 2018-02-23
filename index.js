59bae6695dfa8383e9a7c66b73a86b03c624010d
Eli Draluk <eli.draluk@gmail.com>
2018-02-23 18:58:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,10 +13,9 @@ function currentLine(line){
   if(line.length===0){
     return "The line is currently empty."
   }
-  else{
     tmpStr=tmpStr+"The line is currently: "
-    for(let i=0;i<line.length){
-      tmpStr=(i+1)
+    for(let i=0;i<line.length;i++){
+      tmpStr+=(i+1)+". "+line[i]+", "
     }
-  }
+  return tmpStr
 }
